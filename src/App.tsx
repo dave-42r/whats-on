67b73c940c986053980fecec10ea9095afc64ca2
David Swindells <dave@davidswindells.com>
2021-10-14 22:55:08 +0100

    Adding github pages package to allow for simpler hosting of site.

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          What's on - TV Bland
+          What's on - your TV guide for the 21st century
         </p>
         <p>
           TV Show and web series database. Create personalised schedules. Episode guide, cast, crew and character information.
