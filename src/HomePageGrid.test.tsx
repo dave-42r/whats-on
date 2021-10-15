53a87373d9ed148c519aab55c205940437c7c94a
David Swindells <dave@davidswindells.com>
2021-10-15 12:07:55 +0100

    adding grid and data with tests to check the grid displays as expected with sample data

--- src/HomePageGrid.test.tsx
+++ src/HomePageGrid.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import HomePageGrid from './HomePageGrid';
 import ExampleData from './exampleData.json';
 
@@ -9,8 +9,15 @@ test('Renders Grid title, Last Added Shows', () => {
     expect(firstGridItem).toBeInTheDocument();
 });
 
-test('Renders Grid of Episodes, should contain a card with episode name, image link, rating - titles should match dummy data', () => {
+test('Renders Grid of Episodes, should contain a card with episode image matching sample data', async () => {
+    const {getByAltText} = await render(<HomePageGrid grid-data={ExampleData} />);
+    const image = getByAltText('Good Morning Britain cover image');
+    expect(image).toHaveAttribute('src', 'https://static.tvmaze.com/uploads/images/medium_portrait/321/804657.jpg')
+});
+
+test('Renders Grid of Episodes, should contain a card with an episode rating', async () => {
     render(<HomePageGrid grid-data={ExampleData} />);
-    const firstGridItem = screen.getByText(/Column 1/i);
-    expect(firstGridItem).toBeInTheDocument();
+    const card = screen.getByTestId('0');
+    const rating = within(card).getByText(/rating/i);
+    expect(rating).toHaveTextContent("0/10")
 });
--- src/HomePageGrid.tsx
+++ src/HomePageGrid.tsx
@@ -1,22 +1,48 @@
+import { defaultMaxListeners } from 'events';
+import _ from "lodash";
 import { Responsive, WidthProvider } from 'react-grid-layout';
+import sampleData from './exampleData.json';
 
 function HomePageGrid() {
-    const layout = [
-        { i: 'Column1', x: 0, y: 0, w: 1, h: 2, static: true },
-        { i: 'Column2', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-        { i: 'Column3', x: 4, y: 0, w: 1, h: 2 }
-    ];
-    const layouts = { lg: layout, md: layout};
+     const defaultProps = {
+         className: "layout",
+         isDraggable: false,
+    };
+
+    function generateDOM() {
+        const layout = generateLayout();
+        return _.map(_.range(sampleData.length), function (i) {
+            return (
+                <div key={i} data-grid={layout[i]} data-testid={i.toString()}>
+                    <img alt={sampleData[i].show.name + " cover image"} src={sampleData[i].show.image?.medium} />
+                    <div className="text">Rating:{sampleData[i].show.rating.average ? sampleData[i].show.rating.average + "/10" : "0/10" }</div>
+                    <div className="text">{sampleData[i].show.name}</div>
+                </div>
+            );
+        });
+    }
+
+    function generateLayout() {
+        const p = defaultProps;
+        return _.map(sampleData, function (item, i) {
+            let w : number = _.result(p, "w") || Math.ceil(Math.random() * 4);
+            let y : number = _.result(p, "y") || Math.ceil(Math.random() * 4) + 1;
+            return {
+                x: (i * 2) % 12,
+                y: Math.floor(i / 6) * y,
+                w: w,
+                h: y,
+                i: i.toString()
+            };
+        });
+    }
+
     const ResponsiveGridLayout = WidthProvider(Responsive);
     return (
         <div className="HomePageGrid">
             <h1>Last Added Shows</h1>
-            <ResponsiveGridLayout className="layout" layouts={layouts}
-                breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-                cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}>
-                <div key="Column1">Column 1</div>
-                <div key="Column2">Column 2</div>
-                <div key="Column3">Column 3</div>
+            <ResponsiveGridLayout {...defaultProps}>
+                {generateDOM()}
             </ResponsiveGridLayout>
         </div>
     );
