735b8a878ab99cf44591ae2b295565a779e2eb78
David Swindells <dave@davidswindells.com>
2021-10-15 10:49:17 +0100

    Updating home page grid title and tests

--- src/HomePageGrid.test.tsx
+++ src/HomePageGrid.test.tsx
@@ -3,8 +3,14 @@ import { render, screen } from '@testing-library/react';
 import HomePageGrid from './HomePageGrid';
 import ExampleData from './exampleData.json';
 
-test('Renders Grid of Episodes, should contain a card with episode name, image link, rating - titles should match dummy data', () => {
+test('Renders Grid title, Last Added Shows', () => {
     render(<HomePageGrid grid-data={ExampleData}/>);
+    const firstGridItem = screen.getByText(/last added shows/i);
+    expect(firstGridItem).toBeInTheDocument();
+});
+
+test('Renders Grid of Episodes, should contain a card with episode name, image link, rating - titles should match dummy data', () => {
+    render(<HomePageGrid grid-data={ExampleData} />);
     const firstGridItem = screen.getByText(/Column 1/i);
     expect(firstGridItem).toBeInTheDocument();
 });
