715cf04daa22fb5cef366fddfdb748229cdded3e
dave-42r <82815194+dave-42r@users.noreply.github.com>
2021-10-16 13:04:03 +0100

    Feature/render shows in grid (#1)
    * Updating home page grid title and tests
    
    * adding grid and data with tests to check the grid displays as expected with sample data
    
    * adding missing test for show title
    
    * adding css for RGL
    
    * fixing grid size to sort out most of the layout issues in the grid. Need to add padding etc to the grid.
    
    * updating styling
    
    * adding routing
    
    Co-authored-by: David Swindells <dave@davidswindells.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 import HomePageGrid from './HomePageGrid';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p id="Title">
-          What's on - your TV guide for the 21st century
+    <Router>
+      <div>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/">
+            <div className="App">
+              <header className="App-header">
+                <Link to="/"><p id="Title">
+                  What's on - your TV guide for the 21st century
         </p>
-      </header>
-      <p id="TagLine">
-        TV Show and web series database. Create personalised schedules. Episode guide, cast, crew and character information.
+        </Link>
+              </header>
+              <p id="TagLine">
+                TV Show and web series database. Create personalised schedules. Episode guide, cast, crew and character information.
         </p>
-      <HomePageGrid />
-    </div>
+              <HomePageGrid />
+            </div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
--- src/HomePageGrid.test.tsx
+++ src/HomePageGrid.test.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import HomePageGrid from './HomePageGrid';
 import ExampleData from './exampleData.json';
 
-test('Renders Grid of Episodes, should contain a card with episode name, image link, rating - titles should match dummy data', () => {
+test('Renders Grid title, Last Added Shows', () => {
     render(<HomePageGrid grid-data={ExampleData}/>);
-    const firstGridItem = screen.getByText(/Column 1/i);
+    const firstGridItem = screen.getByText(/last added shows/i);
     expect(firstGridItem).toBeInTheDocument();
+});
+
+test('Renders Grid of Episodes, should contain a card with the title Good Morning Britain', async () => {
+    render(<HomePageGrid grid-data={ExampleData} />);
+    const card = screen.getByTestId('0');
+    expect(card).toHaveTextContent("Good Morning Britain")
+});
+
+test('Renders Grid of Episodes, should contain a card with episode image matching sample data', async () => {
+    const {getByAltText} = await render(<HomePageGrid grid-data={ExampleData} />);
+    const image = getByAltText('Good Morning Britain cover image');
+    expect(image).toHaveAttribute('src', 'https://static.tvmaze.com/uploads/images/medium_portrait/321/804657.jpg')
+});
+
+test('Renders Grid of Episodes, should contain a card with an episode rating', async () => {
+    render(<HomePageGrid grid-data={ExampleData} />);
+    const card = screen.getByTestId('0');
+    const rating = within(card).getByText(/rating/i);
+    expect(rating).toHaveTextContent("0/10")
+});
+
+test('Renders a card with a link to it\'s own show page', async () => {
+    render(<HomePageGrid grid-data={ExampleData} />);
+    const card = screen.getByTestId('0');
+    const linkToShowPage = within(card).findByText(/<a>/);
+    expect(linkToShowPage).toHaveAttribute("href","");
 });
